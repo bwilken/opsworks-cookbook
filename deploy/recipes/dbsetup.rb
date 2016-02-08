598ba03e4b97fd1f63ea3b1bb9ad8ee336fad910
bwilken <brad@ctopowered.com>
2016-02-08 09:35:54 -0600

    getting chef to behave

--- deploy/recipes/dbsetup.rb
+++ deploy/recipes/dbsetup.rb
@@ -5,12 +5,12 @@ node[:deploy].each do |app_name, deploy|
     mode 0660
     group deploy[:group]
 
-    variables(
+    variables({
       :db_host =>     (deploy[:database][:db_host] rescue nil),
       :db_username =>     (deploy[:db_username][:database] rescue nil),
       :db_password => (deploy[:db_password][:database] rescue nil),
       :db_name =>       (deploy[:db_name][:database] rescue nil)
-    )
+    })
 
    only_if do
      File.directory?("#{deploy[:deploy_to]}/current/application/config/")
